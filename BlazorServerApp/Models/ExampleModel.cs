b09eb80bd09921961565824617d3ed0f0bd29618
fahricigdem <fahricigdem34@gmail.com>
2022-01-15 00:04:15 +0100

    form validation

---
+++ BlazorServerApp/Models/ExampleModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class ExampleModel
+{
+	[Required]
+	[StringLength(10, ErrorMessage = "Name is too long.")]
+	public string? Name { get; set; }
+
+	[Required]
+	[StringLength(10, ErrorMessage = "Last Name is too long.")]
+	public string? LastName { get; set; }
+}
+
+
+
+
+
+
